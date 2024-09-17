369c6cb0d2aa09904c997975ec9d6af953d83302
Philipsmithleon <kokolisokoko4@gmail.com>
2024-09-16 22:58:35 -0300

    Codigo Final
    Se agrega
    o	Inicio de sesión.
    o	Registro de usuarios.
    o	Recuperar contraseña.
    o	Modificación de perfil.
    o     Carro de compras
    o     Verificación de y por correo
    o     Formato CRUD

--- todojuegos/settings.py
+++ todojuegos/settings.py
@@ -118,8 +118,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testingdjango1x@gmail.com'
-EMAIL_HOST_PASSWORD = 'jpoc ifhe dcec bnaq'
+EMAIL_HOST_USER = 'ejemplo@gmail.com'
+EMAIL_HOST_PASSWORD = 'j123'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
